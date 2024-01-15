ec16be91f3c6389785324f04b8b5e1a2172ddf7c
albertomolinafelipe <albertomolinafelipe02@gmail.com>
2024-01-14 20:36:39 -0500

    get token and request new token done

--- src/utils/auth.rs
+++ src/utils/auth.rs
@@ -2,11 +2,28 @@ use jsonwebtoken::{decode, DecodingKey, Validation, Algorithm, errors::Result as
 use serde::Deserialize;
 use std::fs;
 
+pub fn process_jwt(token: &str) -> JwtResult<AccessTokenClaims> {
+    let public_key = fs::read_to_string("/keys/jwt_key.pem")
+        .expect("Failed to read public key from file");
+
+    let validation = Validation::new(Algorithm::RS256);
+
+    decode::<AccessTokenClaims>(
+        token,
+        &DecodingKey::from_rsa_pem(public_key.as_bytes())?,
+        &validation,
+    ).map(|data| data.claims)
+}
+
+
 #[derive(Deserialize)]
 pub struct AccessTokenClaims {
     pub exp: i64,
     pub iss: String,
     pub sub: String,
+    #[serde(rename = "preferred_username")]
+    pub preferred_username: String,
+    /*
     iat: i64,
     jti: String,
     aud: String,
@@ -21,35 +38,6 @@ pub struct AccessTokenClaims {
     scope: String,
     sid: String,
     email_verified: bool,
-    #[serde(rename = "preferred_username")]
-    pub preferred_username: String,
     email: String,
-}
-
-#[derive(Deserialize)]
-struct RealmAccess {
-    roles: Vec<String>,
-}
-
-#[derive(Deserialize)]
-struct ResourceAccess {
-    account: AccountRoles,
-}
-
-#[derive(Deserialize)]
-struct AccountRoles {
-    roles: Vec<String>,
-}
-
-pub fn process_jwt(token: &str) -> JwtResult<AccessTokenClaims> {
-    let public_key = fs::read_to_string("/keys/jwt_key.pem")
-        .expect("Failed to read public key from file");
-
-    let validation = Validation::new(Algorithm::RS256);
-
-    decode::<AccessTokenClaims>(
-        token,
-        &DecodingKey::from_rsa_pem(public_key.as_bytes())?,
-        &validation,
-    ).map(|data| data.claims)
+    */
 }
--- src/utils/keycloak.rs
+++ src/utils/keycloak.rs
@@ -1,8 +1,13 @@
+use reqwest;
+use std::env;
+use dotenv::dotenv;
+use serde::Deserialize;
+
+
 pub struct KeycloakClient {
     pub token: Option<String>,
     pub token_expires: i64,
 }
-
 
 impl KeycloakClient {
 
@@ -25,34 +30,122 @@ impl KeycloakClient {
     // TODO
     /// Calls KC for a new access token 
     async fn request_new_token(&self) -> Result<NewTokenResponse, KeycloakError> {
+        dotenv().ok();
+        let client = reqwest::Client::new();
+        let realm_name: String = env::var("KC_REALM_NAME").expect("KC_REALM_NAME must be set");
+        let client_id: String = env::var("KC_CLIENT_ID").expect("KC_CLIENT_ID must be set");
+        let client_secret: String = env::var("KC_CLIENT_SECRET").expect("KC_CLIENT_SECRET must be set");
+
+        // let url = format!("https://auth.salamandra-app.com/admin/realms/{}/users?username={}", realm_name, user_name);
+        let url = format!("http://localhost:8080/realms/{}/protocol/openid-connect/token", realm_name);
+
+        let params = [
+            ("grant_type", "client_credentials"),
+            ("client_id", &client_id),
+            ("client_secret", &client_secret),
+        ];
+
+        match client.get(url)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .form(&params)
+            .send()
+            .await {
+                Ok(response) => {
+                    if !response.status().is_success() {
+                        return Err(KeycloakError::RequestError)
+                    }
+                    match response.json::<NewTokenResponse>().await {
+                        Ok(new_token) => Ok(new_token),
+                        Err(_) => Err(KeycloakError::InternalServerError)
+                    }
+                },
+                Err(_) => Err(KeycloakError::RequestError),
+            }
     }
 
     // TODO
     /// Given uuid, removes user from KC database 
     async fn delete_user(&mut self, user_id: String) -> Result<(), KeycloakError> {
+        Err(KeycloakError::RequestError)
     }
 
-    // TODO
     async fn get_user_info(&mut self, user_name: String) -> Result<UserInfo, KeycloakError> {
+        dotenv().ok();
+        let access_token: &String = self.get_token().await?;
+        let client = reqwest::Client::new();
+        let realm_name: String = env::var("KC_REALM_NAME").expect("KC_REALM_NAME must be set");
+
+        // let url = format!("https://auth.salamandra-app.com/admin/realms/{}/users?username={}", realm_name, user_name);
+        let url = format!("http://localhost:8080/admin/realms/{}/users?username={}", realm_name, user_name);
+        match client.get(url)
+            .header("Authorization", format!("Bearer {}", access_token))
+            .send()
+            .await {
+                Ok(response) => {
+                    if !response.status().is_success() {
+                        return Err(KeycloakError::RequestError)
+                    }
+                    match response.json::<UserInfo>().await {
+                        Ok(user_info) => Ok(user_info),
+                        Err(_) => Err(KeycloakError::InternalServerError)
+                    }
+                },
+                Err(_) => Err(KeycloakError::RequestError),
+            }
+
     }
-}
-
-struct NewTokenResponse {
-    token: String,
-    expires_in: i64,
-}
-
-// TODO
-pub struct UserInfo {
-    // no me acuerdo
 }
 
 enum KeycloakError {
-    ConnectionError,
-    InvalidClientCredentials,
-    Unauthorized,
-    TokenExpired,
-    ResourceNotFound,
     RequestError,
+    InternalServerError,
 }
 
+#[derive(Deserialize, Debug)]
+struct NewTokenResponse {
+    token: String,
+    expires_in: i64,
+    /*
+    refresh_expires_in: i64,
+    token_type: String,
+    #[serde(rename = "not-before-policy")]
+    not_before_policy: i64,
+    scope: String,
+    */
+}
+
+
+#[derive(Deserialize, Debug)]
+struct UserInfo {
+    #[serde(rename = "emailVerified")]
+    email_verified: bool,
+    /*
+    id: String,
+    #[serde(rename = "createdTimestamp")]
+    created_timestamp: i64,
+    username: String,
+    enabled: bool,
+    totp: bool,
+    email: String,
+    #[serde(rename = "disableableCredentialTypes")]
+    disableable_credential_types: Vec<String>,
+    #[serde(rename = "requiredActions")]
+    required_actions: Vec<String>,
+    #[serde(rename = "notBefore")]
+    not_before: i32,
+    access: Access,
+    */
+}
+
+/*
+#[derive(Deserialize, Debug)]
+struct Access {
+    #[serde(rename = "manageGroupMembership")]
+    manage_group_membership: bool,
+    view: bool,
+    #[serde(rename = "mapRoles")]
+    map_roles: bool,
+    impersonate: bool,
+    manage: bool,
+}
+*/
